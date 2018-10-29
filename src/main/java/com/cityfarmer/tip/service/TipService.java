603bf7bc06b0f06f7baaab5934ed8e70117c79f5
kim <rlaqudrhks258@naver.com>
2018-10-30 01:31:31 +0900

    18.10.30- 꿀팁 게시판 전체게시글조회

--- src/main/java/com/cityfarmer/tip/service/TipService.java
+++ src/main/java/com/cityfarmer/tip/service/TipService.java
@@ -1,5 +1,12 @@
 package com.cityfarmer.tip.service;
 
-public interface TipService {
+import java.util.List;
 
+import com.cityfarmer.repository.domain.TipBoard;
+
+public interface TipService {
+	//글등록
+	/*void insertBoard(TipBoard board);*/
+	// 전체 게시글 조회
+	List<TipBoard> selectBoard();
 }
