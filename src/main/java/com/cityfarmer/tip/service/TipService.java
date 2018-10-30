53cb1c896480fa43ca0a70ef676c7c951f3faaa6
kim <rlaqudrhks258@naver.com>
2018-10-30 22:17:23 +0900

    18.10.30-김병관 글등록, 상세조회

--- src/main/java/com/cityfarmer/tip/service/TipService.java
+++ src/main/java/com/cityfarmer/tip/service/TipService.java
@@ -6,7 +6,10 @@ import com.cityfarmer.repository.domain.TipBoard;
 
 public interface TipService {
 	//글등록
-	/*void insertBoard(TipBoard board);*/
+	void insertBoard(TipBoard board);
 	// 전체 게시글 조회
 	List<TipBoard> selectBoard();
+	TipBoard selectBoardByNo(int no);
+
+	
 }
